16976fcb661e2fb80246bbe52a71c74a7a9135c6
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-08 22:20:54 +0530

    Added: Hash Map using linked list

---
+++ HashMain.java
@@ -0,0 +1,19 @@
+package com.bridgelabzhash;
+
+public class HashMain {
+
+	public static void main(String[] args) {
+		
+		HashTable<Integer, String> hash = new HashTable<Integer, String>();
+
+		hash.insertNode(1, "to");
+		hash.insertNode(2, "be");
+		hash.insertNode(3, "or");
+		hash.insertNode(4, "not");
+		hash.insertNode(5, "to");
+		hash.insertNode(6, "be");
+		hash.print();
+
+	}
+
+}
---
+++ HashTable.java
@@ -0,0 +1,111 @@
+package com.bridgelabzhash;
+
+public class HashTable<E, K> 
+{
+	
+	Node<E, K> head = null, current, temp;
+
+	@SuppressWarnings("hiding")
+	class Node<E, K> 
+	{
+		E key;
+		K data;
+		Node<E, K> next;
+
+		Node(E k, K value) 
+		{
+			key = k;
+			data = value;
+			next = null;
+		}
+	}
+	
+	
+	/**
+	 * This method is used to insert the data
+	 * 
+	 * @param data This is the value of the data
+	 */
+	public void insertNode(E keys, K data) 
+	{
+		Node<E, K> newnode = new Node<E, K>(keys, data);
+		if (head == null) 
+		{
+			head = newnode;
+			current = head;
+		} 
+		
+		else {
+			temp = head;
+			while (temp != null) 
+			{
+				if (temp.key == keys) 
+				{
+					temp.key = keys;
+					temp.data = data;
+				}
+				temp = temp.next;
+			}
+			head = newnode;
+			head.next = current;
+			current = head;
+		}
+	}
+
+	/**
+	 * This method will check the number of times the data is present in the linked list
+	 * It increments the count, the number of times data is present
+	 */
+	public void frequency(K data) 
+	{
+		int count = 0;
+		temp = head;
+		while (temp != null) 
+		{
+			if (temp.data == data) 
+			{
+				count++;
+			}
+			temp = temp.next;
+		}
+		System.out.println("Frequency of '" + data + "' is :" + count);
+	}
+
+	//this method is used to check if linked list is empty 
+	
+	public Boolean isEmpty() 
+	{
+		temp = head;
+		if (temp == null) 
+		{
+			return true;
+		} 
+		else 
+		{
+			return false;
+		}
+	}
+
+	//this method will print the linked list 
+	
+	public void print() 
+	{
+		Node<E, K> tempnode = head;
+		if (!isEmpty()) 
+		{
+			while (tempnode.next != null) 
+			{
+				System.out.println(tempnode.data);
+				frequency(tempnode.data);
+				tempnode = tempnode.next;
+			}
+			System.out.println(tempnode.data);
+			frequency(tempnode.data);
+		} 
+		else 
+		{
+			System.out.println("Empty Queue");
+		}
+	}
+	
+}
